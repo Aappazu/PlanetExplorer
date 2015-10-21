75f62269804713ace774bd3a5e36529553af2ffa
somename <someemail>
2015-10-21 16:34:46 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -118,6 +118,24 @@ public class PlanetExplorer {
 		}
 	}
 	
+	public void moveBackward(){
+		if(explorerXCoord < maxXCoord && explorerFacing == "W"){
+			explorerXCoord++;
+		}
+		else if(explorerXCoord > 0 && explorerFacing == "E"){
+			explorerXCoord--;
+		}
+		else if(explorerYCoord < maxYCoord && explorerFacing == "N"){
+			explorerYCoord++;
+		}
+		else if(explorerYCoord > 0 && explorerFacing == "S"){
+			explorerYCoord--;
+		}
+		else{
+			
+		}
+	}
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
