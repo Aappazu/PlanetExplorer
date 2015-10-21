b939e51b7929b2e07a2e71a012153499ad8bd0cc
somename <someemail>
2015-10-21 16:06:04 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -2,19 +2,14 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.unioulu.tol.sqat2015.planetExplorer.*;
 
 public class TestPlanetExplorer {
-	
-	@Before
-	public void setup(){
-		PlanetExplorer pExplorer = new PlanetExplorer(100,100);
-	}
 
 	@Test
 	public void landingCompletedRoute() {
+		PlanetExplorer pExplorer = new PlanetExplorer(100,100);
 		
 		pExplorer.setLocation(5, 5,"E");
 		
@@ -23,6 +18,7 @@ public class TestPlanetExplorer {
 	
 	@Test
 	public void getLandingLocation(){
+		PlanetExplorer pExplorer = new PlanetExplorer(100,100);
 		
 		pExplorer.setLocation(5,5, "E");
 		
@@ -33,6 +29,7 @@ public class TestPlanetExplorer {
 	
 	@Test
 	public void currentLocationReturn(){
+		PlanetExplorer pExplorer = new PlanetExplorer(10,10);
 		
 		pExplorer.setLocation(5, 5, "E");
 		
@@ -42,6 +39,7 @@ public class TestPlanetExplorer {
 	@Test
 	public void turnLeftFromFacingEast(){
 		
+		PlanetExplorer pExplorer = new PlanetExplorer(10,10);
 		
 		pExplorer.setLocation(5, 5, "E");
 		
@@ -53,11 +51,26 @@ public class TestPlanetExplorer {
 	@Test
 	public void turnRightFromFacingEast(){
 		
+		PlanetExplorer pExplorer = new PlanetExplorer(10,10);
+		
 		pExplorer.setLocation(5, 5, "E");
 		
         pExplorer.turnRight();
 		
 		assertEquals("(5,5,S)", pExplorer.getCurrentLocation());
+	}
+	
+	@Test
+	public void moveForwardFromZeroZeroSouth(){
+		
+		PlanetExplorer pExplorer = new PlanetExplorer(10,10);
+		
+		pExplorer.setLocation(0, 0, "S");
+		
+		pExplorer.moveForward();
+		
+		assertEquals("(0,1,S)", pExplorer.getCurrentLocation());
+		
 	}
 	
 }
