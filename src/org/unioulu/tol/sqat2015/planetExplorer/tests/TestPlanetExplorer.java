59bb91d275fcf772321ebb597ef146a9e9878257
somename <someemail>
2015-10-21 16:21:00 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -8,6 +8,15 @@ import org.unioulu.tol.sqat2015.planetExplorer.*;
 public class TestPlanetExplorer {
 
 	@Test
+	public void currentLocationReturn(){
+		PlanetExplorer pExplorer = new PlanetExplorer(10,10);
+		
+		pExplorer.setLocation(5, 5, "E");
+		
+		assertEquals("(5,5,E)", pExplorer.getCurrentLocation());
+	}
+	
+	@Test
 	public void landingCompletedRoute() {
 		PlanetExplorer pExplorer = new PlanetExplorer(100,100);
 		
@@ -27,14 +36,7 @@ public class TestPlanetExplorer {
 		assertEquals("(0,0,N)", pExplorer.getCurrentLocation());
 	}
 	
-	@Test
-	public void currentLocationReturn(){
-		PlanetExplorer pExplorer = new PlanetExplorer(10,10);
-		
-		pExplorer.setLocation(5, 5, "E");
-		
-		assertEquals("(5,5,E)", pExplorer.getCurrentLocation());
-	}
+
 	
 	@Test
 	public void turnLeftFromFacingEast(){
